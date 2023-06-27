8095ac0b516c12a6b3f6153835ea1baebcc633d5
Bodi Yang <byang89@wisc.edu>
2023-06-27 13:33:27 -0400

    add taxcalcio class

--- taxcalcpayroll/__init__.py
+++ taxcalcpayroll/__init__.py
@@ -4,5 +4,6 @@ Specify what is available to import from the taxcalc-payroll package.
 from taxcalcpayroll.calcfunctions import *
 from taxcalcpayroll.calculator import *
 from taxcalcpayroll.tcp import *
+from taxcalcpayroll.taxcalcio import *
 
 __version__ = '0.1.0'
