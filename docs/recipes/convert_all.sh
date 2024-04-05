b26b9c41617b089ab91837c1c03dc14572dd75da
Bodi Yang <byang89@wisc.edu>
2024-04-05 15:34:13 -0400

    add new receipes

--- docs/recipes/convert_all.sh
+++ docs/recipes/convert_all.sh
@@ -1,8 +1,3 @@
-jupytext recipe00.ipynb --to myst -o md_src/recipe00.md
-jupytext recipe01.ipynb --to myst -o md_src/recipe01.md
-jupytext recipe02.ipynb --to myst -o md_src/recipe02.md
-jupytext recipe03.ipynb --to myst -o md_src/recipe03.md
-jupytext recipe04.ipynb --to myst -o md_src/recipe04.md
-jupytext recipe04_pandas.ipynb --to myst -o md_src/recipe04_pandas.md
-jupytext recipe05.ipynb --to myst -o md_src/recipe05.md
-jupytext recipe06.ipynb --to myst -o md_src/recipe06.md
+jupytext recipe1.ipynb --to myst -o md_src/recipe1.md
+jupytext recipe2.ipynb --to myst -o md_src/recipe2.md
+jupytext recipe3.ipynb --to myst -o md_src/recipe3.md
